077f9aa438af305f3e1b6a102744dba8090c1d72
mariam <mariam.abdussalam@andela.com>
2017-04-07 18:51:51 +0100

    feature(reducer): add document reducer

---
+++ client/src/reducers/roles.js
@@ -0,0 +1,12 @@
+import { GET_ROLES, CREATE_ROLE} from '../actions/types';
+
+export default (state = [], action) => {
+  switch(action.type) {
+  case GET_ROLES:
+     return action.roles;
+
+  case CREATE_ROLE:
+      return [...state, Object.assign({}, action.role)];
+  default: return state;
+  }
+}
