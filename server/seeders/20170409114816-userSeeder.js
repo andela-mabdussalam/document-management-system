9df8e1835d119f1cfa639273756b452c829c3bd2
mariam <mariam.abdussalam@andela.com>
2017-04-09 12:55:48 +0100

    fix(heroku): Add seeder file

---
+++ server/seeders/20170409114816-userSeeder.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+      return queryInterface.bulkInsert('Users', [{
+        userName: 'admin',
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: 'mannah@dms.com',
+        password: bcrypt.hashSync('admin'),
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userName: 'regular',
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: 'mannaher@dms.com',
+        password: bcrypt.hashSync('regular'),
+        roleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]);
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
