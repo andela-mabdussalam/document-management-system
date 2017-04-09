c771e71585f907fe44f28254cc7b38dd17db93ae
mariam <mariam.abdussalam@andela.com>
2017-04-09 08:20:41 +0100

    Change buildpack output directory

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ export default {
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
+    publicPath: '/public/',
     filename: 'bundle.js'
   },
   devServer: {
